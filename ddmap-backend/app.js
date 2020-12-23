a1d96801b98570b40c63e20b68b7e15bcaa4d24c
Nuung <qlgks1@naver.com>
2020-12-23 20:47:28 +0900

    add app js config setting

--- ddmap-backend/app.js
+++ ddmap-backend/app.js
@@ -3,44 +3,44 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
-const session = require('express-session'); 
+const session = require('express-session');
 const flash = require('connect-flash');
 const cors = require('cors');
 const escapeJSON = require('escape-json-node'); // 
-const authRouter = require('./src/routes/auth'); 
+const authRouter = require('./src/routes/auth');
 
-// ORM - sequelize
+//−−−−−−−−−−−−−−−−− ORM - sequelize Setting −−−−−−−−−−−−−−−−−//
 const sequelize = require('./src/models').sequelize;
-sequelize.sync(); 
+sequelize.sync();
 
 
-//add .env file 
-require('dotenv').config(); 
-
+//−−−−−−−−−−−−−−−−− APP config Setting −−−−−−−−−−−−−−−−−//
+require('dotenv').config(); // add .env file 
 const app = express();
 
 app.use(logger('dev'));
-
 app.use(express.json()); // body-parser setting ~ express include body-parser from 4.X version
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(cors()); // CORS 설정
-
-//app session 
-
-app.use(session({
-  resave: false, 
-  saveUninitialized: false, 
+app.use(session({ // app session 
+  resave: false,
+  saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
   cookie: {
-    httpOnly:true, 
-    secure: false, 
+    httpOnly: true,
+    secure: false,
   },
 }));
+app.use(cors({  // CORS 설정
+  origin: true,
+  credentials: true
+}));
+// app.set('jwt-secret', appConfig.parsed.JWT_SECRET); // set the secret key variable for jwt
 
-app.use(flash()); 
+app.use(flash());
 
-app.use('/auth', authRouter); 
+//−−−−−−−−−−−−−−−−− API Routing Setting −−−−−−−−−−−−−−−−−//
+app.use('/auth', authRouter);
 
 //image 사용 할 수 있게 만들어주는 static 
 app.use('/img', express.static('uploads'));
@@ -48,32 +48,27 @@ app.use('/img', express.static('uploads'));
 
 
 
+//−−−−−−−−−−−−−−−−− Other Config Setting −−−−−−−−−−−−−−−−−//
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-// view Engine 
-
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error pag
   res.status(err.status || 500);
- 
+
   res.json({
     message: err.message,
     error: err
   });
 
 });
-
-
-
 
 module.exports = app;
 
