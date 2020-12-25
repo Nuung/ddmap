de672b66eb15cadf149ac7545eab407f0c61b868
Nuung <qlgks1@naver.com>
2020-12-26 02:40:57 +0900

    rebase clear

--- ddmap-backend/app.js
+++ ddmap-backend/app.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 const escapeJSON = require('escape-json-node');
 
 // require Routers
-const authRouter = require('./src/routes/auth');
 const userRouter = require('./src/routes/userRouter');
 const toiletRouter = require('./src/routes/toiletRouter');
 
@@ -46,8 +45,7 @@ app.set('jwt-secret', process.env.JWT_SECRET); // set the secret key variable fo
 
 
 //−−−−−−−−−−−−−−−−− API Routing Setting −−−−−−−−−−−−−−−−−//
-app.use('/auth', authRouter);
-userRouter(app); // 위와 차이점은 appjs에서 만든 app을 그대로 이용, routing 내부에서 처리 바로 가능
+userRouter(app); 
 toiletRouter(app);
 
 // image 사용 할 수 있게 만들어주는 static 
